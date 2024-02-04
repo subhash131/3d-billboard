9e5e6ee7943f76cb33962cc7cf4580ed225191e2
subhash131 <subhashnayak131@gmail.com>
2024-02-04 20:18:42 +0530

    initial setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} w-screen h-screen overflow-hidden`}>
+        {children}
+      </body>
     </html>
   );
 }
